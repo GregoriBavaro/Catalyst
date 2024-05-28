9e32bd6309d056a2022806c7fa10f901774acbe1
Gregori <greg.gego@gmail.com>
2024-05-28 11:19:02 +0200

    cards added

---
+++ components/ui/Cards.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+
+import classes from "./Cards.module.scss";
+
+interface CardsProps {
+  data: { id: number; icon: string; title: string; description: string; path: string }[];
+  sectionName: string;
+}
+const Cards: React.FC<CardsProps> = ({ data, sectionName }) => {
+  return (
+    <section className={classes.cardsWrapper}>
+      <h4>{sectionName}</h4>
+      <ul>
+        {data.map(({ id, icon, title, description, path }) => (
+          <li key={id}>
+            <Link href={path}>
+              {icon && <span>{icon}</span>}
+              <h6>{title}</h6>
+              <p>{description}</p>
+              <div className={classes.learnMore}>
+                <span className={classes.circle} aria-hidden="true">
+                  <span className={`${classes.icon} ${classes.arrow}`} />
+                </span>
+                <p>Learn more</p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Cards;
