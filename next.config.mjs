57c8e251f0da00e5be541c5516803f4a436427cb
Gregori <greg.gego@gmail.com>
2024-07-04 16:37:58 +0200

    configured next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
   // experimental: {
   //   appDir: true,
   // },
+  // trailingSlash: true,
   images: { formats: ["image/avif", "image/webp"] },
+  rewrites: [
+    {
+      source: "/:path",
+      destination: "/:path/index.html",
+    },
+  ],
 };
 
 export default nextConfig;
