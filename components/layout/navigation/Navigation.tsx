b90650030b7a488b9f0d93884a86e43d38a75702
Gregori <greg.gego@gmail.com>
2024-07-04 12:11:26 +0200

    navigation bar for mobile and desktop changed on window size

--- components/layout/navigation/Navigation.tsx
+++ components/layout/navigation/Navigation.tsx
@@ -3,6 +3,7 @@
 import { motion as m } from "framer-motion";
 import useScrollPosition from "../../../hooks/use-ScrollPosition";
 import useScrollDirection from "../../../hooks/use-ScrollDirection";
+import useWindowSize from "../../../hooks/use-WindowSize";
 
 import HamburgerMenu from "../../ui/hamburger/HamburgerMenu";
 import Logo from "../logo/Logo";
@@ -10,15 +11,19 @@ import Logo from "../logo/Logo";
 import classes from "./Navigation.module.scss";
 
 const Navigation = () => {
+  const size = useWindowSize();
   const scrollPosition = useScrollPosition();
   const scrollDirection = useScrollDirection();
 
   const navigationStyles = {
     animate: {
-      top: scrollPosition > 50 && scrollDirection === "down" ? "-8.3rem" : "0",
+      top:
+        scrollPosition > 50 && scrollDirection === "down" && (size.width ?? 768) > 768
+          ? "-8.3rem"
+          : "0",
       background:
-        scrollPosition > 0
-          ? "linear-gradient(0deg, rgba(1,19,31,0) 0%, rgba(1,19,31,0.9156454818255427) 47%, rgba(1,19,31,1) 70%)"
+        scrollPosition > 0 || (size.width ?? 768) < 768
+          ? "linear-gradient(0deg, rgba(1,19,31,0) 0%, rgba(1,19,31,0.9156454818255427) 47%, rgba(1,19,31,1) 50%)"
           : "linear-gradient(0deg, rgba(1,19,31,0) 0%, rgba(1,19,31,0) 47%, rgba(1,19,31,0) 70%)",
     },
     transition: { duration: 0.5 },
