599beea33966682dd0163c91a29d0c3c37021365
Gregori <greg.gego@gmail.com>
2024-07-14 12:47:35 +0200

    robot & sitemap logic changed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://catalyst.mk",
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   sitemapSize: 5000,
   changefreq: "daily",
   priority: 0.7,
   robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
     additionalSitemaps: [
-      'https://www.catalyst.mk/sitemap-portfolio.xml',
-      'https://www.catalyst.mk/sitemap-services.xml',
+      "https://www.catalyst.mk/sitemap-portfolio.xml",
+      "https://www.catalyst.mk/sitemap-services.xml",
     ],
+    // Optional, if you want to add comments or specific directives
+    // include more fields as necessary
   },
 };
